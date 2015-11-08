d65ed542886413ac5b718e7371aa3ae5b077e3c2
Sean <toranos@gmail.com>
2015-11-08 21:05:03 +1100

    Enable preferences in firefox addon

--- src/firefox/index.js
+++ src/firefox/index.js
@@ -9,22 +9,28 @@ function dummy(text, callback) {
 
 exports.dummy = dummy;
 
+var preferences = require("sdk/simple-prefs").prefs;
+
 // Import the page-mod API
 var pageMod = require("sdk/page-mod");
  
-pageMod.PageMod({
-    include: "https://www.youtube.com/watch?*",
-    contentScriptFile: self.data.url("Youtube_Gaming_Redirect.user.js")
-});
-
-pageMod.PageMod({
-    include: "*",
-    exclude: "https://www.youtube.com/*",
-    contentScriptFile: self.data.url("Youtube_Gaming_Linker.user.js")
-});
-
-pageMod.PageMod({
-    include: "https://feedly.com/*",
-    contentScriptFile: [self.data.url("jquery-2.1.4.min.js"),
-                        self.data.url("Youtube_Gaming_Feedly.user.js")]
-});
+if (preferences["redirect"]){
+    pageMod.PageMod({
+        include: "https://www.youtube.com/watch?*",
+        contentScriptFile: self.data.url("Youtube_Gaming_Redirect.user.js")
+    });
+}
+if (preferences["linker"]){
+    pageMod.PageMod({
+        include: "*",
+        exclude: "https://www.youtube.com/*",
+        contentScriptFile: self.data.url("Youtube_Gaming_Linker.user.js")
+    });
+}
+if (preferences["feedly"]){
+    pageMod.PageMod({
+        include: "https://feedly.com/*",
+        contentScriptFile: [self.data.url("jquery-2.1.4.min.js"),
+                            self.data.url("Youtube_Gaming_Feedly.user.js")]
+    });
+}
