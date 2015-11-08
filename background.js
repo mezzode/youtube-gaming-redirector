46ebaa7f6b73e2bca751ac2f68b04eb0e1f8bcbc
Sean <toranos@gmail.com>
2015-11-08 17:28:14 +1100

    Selectively apply scripts based on URL

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    console.log("test");
     if (changeInfo.status == 'complete'){
+        console.log("test");
         chrome.storage.sync.get({
             redirect: true,
             linker: true,
             feedly: true
         }, function(items) {
-            if (items.redirect) {
-                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Redirect.user.js"});
+            if (items.redirect && tab.url.match("^https:\/\/www\.youtube\.com\/*")) {
+                chrome.tabs.executeScript(tabId, {file: "Youtube_Gaming_Redirect.user.js"});
             }
-            if (items.linker) {
-                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Linker.user.js"});
+            if (items.linker && !(tab.url.match("^https:\/\/www\.youtube\.com\/*"))) {
+                chrome.tabs.executeScript(tabId, {file: "Youtube_Gaming_Linker.user.js"});
             }
-            if (items.feedly){
-                chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
-                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Feedly.user.js"});
+            if (items.feedly && tab.url.match("^https:\/\/feedly\.com\/*")){
+                chrome.tabs.executeScript(tabId, {file: "jquery-2.1.4.min.js"});
+                chrome.tabs.executeScript(tabId, {file: "Youtube_Gaming_Feedly.user.js"});
             }
         });
     }
