d4932c07c854c4bae2ffdd856b519f76a9adb31c
Sean <toranos@gmail.com>
2015-11-08 17:06:57 +1100

    Fix background.js to inject per new page

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
-chrome.storage.sync.get({
-    redirect: true,
-    linker: true,
-    feedly: true
-}, function(items) {
-    console.log("test");
-    if (items.redirect) {
-        chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Redirect.user.js"});
-    }
-    if (items.linker) {
-        chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Linker.user.js"});
-    }
-    if (items.feedly){
-        chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
-        chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Feedly.user.js"});
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete'){
+        chrome.storage.sync.get({
+            redirect: true,
+            linker: true,
+            feedly: true
+        }, function(items) {
+            if (items.redirect) {
+                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Redirect.user.js"});
+            }
+            if (items.linker) {
+                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Linker.user.js"});
+            }
+            if (items.feedly){
+                chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
+                chrome.tabs.executeScript(null, {file: "Youtube_Gaming_Feedly.user.js"});
+            }
+        });
     }
 });
